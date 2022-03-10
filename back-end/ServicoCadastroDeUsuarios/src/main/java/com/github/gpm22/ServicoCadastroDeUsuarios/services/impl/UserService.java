b91937abb7932537362d410f01db992989868208
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-03-10 01:58:47 -0300

    alterando o método update

--- back-end/ServicoCadastroDeUsuarios/src/main/java/com/github/gpm22/ServicoCadastroDeUsuarios/services/impl/UserService.java
+++ back-end/ServicoCadastroDeUsuarios/src/main/java/com/github/gpm22/ServicoCadastroDeUsuarios/services/impl/UserService.java
@@ -26,7 +26,7 @@ import lombok.extern.log4j.Log4j2;
 public class UserService implements IUserService {
 
 	@Autowired
-	private PasswordEncoder passwordEncoder = null;
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private IUserRepository userRepository;
@@ -46,47 +46,19 @@ public class UserService implements IUserService {
 	}
 
 	@Override
-	public UserEntity insert(UserEntity user) throws DataIntegrityViolationException {
-		try {
-			user.setPassword(passwordEncoder.encode(user.getPassword()));
-			return userRepository.insert(user);
-		} catch (DataIntegrityViolationException e) {
-			if (e.getMessage().contains("PUBLIC.USERS(USER_CPF)")) {
-				throw new DataIntegrityViolationException("O cpf \"" + user.getCpf() + "\" já foi utilizado");
-			}
-
-			if (e.getMessage().contains("PUBLIC.EMAILS(ADRESS_EMAIL)")) {
-
-				Set<EmailEntity> emails = new HashSet<EmailEntity>(emailService.getAll());
-
-				for (EmailEntity email : user.getEmails()) {
-					if (emails.contains(email)) {
-						throw new DataIntegrityViolationException(
-								"O email \"" + email.getEmail() + "\" já foi utilizado");
-					}
-				}
-
-			}
-
-			throw new DataIntegrityViolationException(
-					"O nome de usuário \"" + user.getUsername() + "\" já foi utilizado");
-		}
-	}
-	
-	@Override
 	public UserEntity getUserByCpf(String cpf) {
-		Optional<UserEntity> user = userRepository.findById((String) cpf);
+		Optional<UserEntity> user = getById((String) cpf);
 
 		if (user.isEmpty()) {
 			throw new IllegalArgumentException("O usuário com CPF " + cpf + " não existe!");
 		}
-		
+
 		return user.get();
 	}
 
 	@Override
-	public Optional<UserEntity> getById(Object cpf) {
-		return userRepository.findById((String) cpf);
+	public Optional<UserEntity> getById(Object id) {
+		return userRepository.findById((String) id);
 	}
 
 	@Override
@@ -94,57 +66,89 @@ public class UserService implements IUserService {
 		object.getAdress().getUsers().remove(object);
 		object.getTelephones().forEach((telephone) -> telephone.getUsers().remove(object));
 		userRepository.remove(object);
-		adressService.clean(object);
-		telephoneService.clean(object);
+		adressService.removeIfOrphan(object);
+		telephoneService.removeOrhans(object);
 		return object;
+	}
+
+	@Override
+	public UserEntity insert(UserEntity user) throws DataIntegrityViolationException {
+		try {
+			encodePassword(user);
+			return userRepository.insert(user);
+		} catch (DataIntegrityViolationException e) {
+			throw insertOrUpdateErro(e, user);
+		}
+	}
+	
+	private void encodePassword(UserEntity user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+	}
+
+	private DataIntegrityViolationException insertOrUpdateErro(DataIntegrityViolationException e, UserEntity user)
+			throws DataIntegrityViolationException {
+		if (e.getMessage().contains("PUBLIC.USERS(USER_CPF)")) {
+			return new DataIntegrityViolationException("O cpf \"" + user.getCpf() + "\" já foi utilizado");
+		}
+
+		if (e.getMessage().contains("PUBLIC.EMAILS(ADRESS_EMAIL)")) {
+
+			Set<EmailEntity> emails = new HashSet<EmailEntity>(emailService.getAll());
+			log.error("erro email: " + e.getMessage());
+
+			for (EmailEntity email : user.getEmails()) {
+				if (emails.contains(email)) {
+					return new DataIntegrityViolationException("O email \"" + email.getEmail() + "\" já foi utilizado");
+				}
+			}
+
+		}
+		
+		if (e.getMessage().contains("PUBLIC.USERS(USER_USERNAME)")) {
+			return new DataIntegrityViolationException("O nome de usuário \"" + user.getUsername() + "\" já foi utilizado");
+		}
+
+		return e;
 	}
 
 	@Override
 	public UserEntity update(UserEntity user) throws DataIntegrityViolationException {
 		try {
 
-			UserEntity userOriginal = userRepository.findById(user.getCpf()).get();
+			UserEntity originalUser = userRepository.findById(user.getCpf()).get();
 
-			if (user.equals(userOriginal) && user.getTelephones().equals(userOriginal.getTelephones())
-					&& user.getEmails().equals(userOriginal.getEmails())
-					&& user.getAdress().equals(userOriginal.getAdress())) {
+			if (user.fullyEquals(originalUser)) {
 				return user;
 			}
-
-			if (!user.getPassword().equals(userOriginal.getPassword())) {
-				user.setPassword(passwordEncoder.encode(user.getPassword()));
+			
+			if(passwordChanged(user, originalUser)) {
+				encodePassword(user);
 			}
 
-			if (user.getEmails().size() < userOriginal.getEmails().size()) {
-
-				Set<EmailEntity> emailsExcluded = new HashSet<>(userOriginal.getEmails());
-
-				emailsExcluded.removeAll(user.getEmails());
-
-				emailsExcluded.forEach((email) -> {
-					emailService.remove(email);
-				});
-				userOriginal.setEmails(user.getEmails());
-			}
+			removeOrfhanEmails(user, originalUser);
 
 			return userRepository.update(user);
 		} catch (DataIntegrityViolationException e) {
 			e.printStackTrace();
+			throw insertOrUpdateErro(e, user);
+		}
+	}
 
-			if (e.getMessage().contains("PUBLIC.EMAILS(ADRESS_EMAIL)")) {
+	private boolean passwordChanged(UserEntity user, UserEntity originalUser) {
+		return !user.getPassword().equals(originalUser.getPassword());
+	}
 
-				Set<EmailEntity> emails = new HashSet<EmailEntity>(emailService.getAll());
+	private void removeOrfhanEmails(UserEntity user, UserEntity originalUser) {
+		if (!user.getEmails().equals(originalUser.getEmails())) {
 
-				for (EmailEntity email : user.getEmails()) {
-					if (emails.contains(email)) {
-						throw new DataIntegrityViolationException("O email \"" + email + "\" já foi utilizado");
-					}
-				}
+			Set<EmailEntity> emailsExcluded = new HashSet<>(originalUser.getEmails());
 
-			}
+			emailsExcluded.removeAll(user.getEmails());
 
-			throw new DataIntegrityViolationException(
-					"O nome de usuário \"" + user.getUsername() + "\" já foi utilizado");
+			emailsExcluded.forEach((email) -> {
+				emailService.remove(email);
+			});
+			originalUser.setEmails(user.getEmails());
 		}
 	}
 
@@ -167,7 +171,7 @@ public class UserService implements IUserService {
 		if (user.isEmpty()) {
 			throw new IllegalArgumentException("O usuário " + userName + " não existe!");
 		}
-		
+
 		return user.get();
 	}
 
