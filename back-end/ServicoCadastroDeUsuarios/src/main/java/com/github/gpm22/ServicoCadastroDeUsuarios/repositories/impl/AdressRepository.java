bbf539c9fe12a091c93cbd518d9203d14515d4f0
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-03-14 01:33:10 -0300

    abstraindo para outro método a execução da query do findExistingAdress

--- back-end/ServicoCadastroDeUsuarios/src/main/java/com/github/gpm22/ServicoCadastroDeUsuarios/repositories/impl/AdressRepository.java
+++ back-end/ServicoCadastroDeUsuarios/src/main/java/com/github/gpm22/ServicoCadastroDeUsuarios/repositories/impl/AdressRepository.java
@@ -67,16 +67,19 @@ public class AdressRepository implements IAdressRepository {
 	public Optional<AdressEntity> findExistingAdress(AdressEntity adress) {
 
 		try {
-			String sql = "Select t from AdressEntity t " + "where t.cep = :cep " + "and t.publicPlace = :publicPlace "
-					+ "and t.district = :district " + "and t.city=:city " + "and t.uf = :uf "
-					+ "and t.complement = :complement";
-			return Optional.of((AdressEntity) entityManager.createQuery(sql).setParameter("cep", adress.getCep())
-					.setParameter("publicPlace", adress.getPublicPlace()).setParameter("district", adress.getDistrict())
-					.setParameter("city", adress.getCity()).setParameter("uf", adress.getUf())
-					.setParameter("complement", adress.getComplement()).getSingleResult());
+			return executeSqlQueryToFindExistingAdress(adress);
 		} catch (NoResultException e) {
 			return Optional.empty();
 		}
 	}
 
+	private Optional<AdressEntity> executeSqlQueryToFindExistingAdress(AdressEntity adress) {
+		String sql = "Select t from AdressEntity t " + "where t.cep = :cep " + "and t.publicPlace = :publicPlace "
+				+ "and t.district = :district " + "and t.city=:city " + "and t.uf = :uf "
+				+ "and t.complement = :complement";
+		return Optional.of((AdressEntity) entityManager.createQuery(sql).setParameter("cep", adress.getCep())
+				.setParameter("publicPlace", adress.getPublicPlace()).setParameter("district", adress.getDistrict())
+				.setParameter("city", adress.getCity()).setParameter("uf", adress.getUf())
+				.setParameter("complement", adress.getComplement()).getSingleResult());
+	}
 }
